82857b7a06f24bcbadec65a35a3643b9b1a500f6
Sercan Degirmenci <degirmencisercan@gmail.com>
2016-08-01 11:47:32 +0300

    disable uglify

--- tasks/html.js
+++ tasks/html.js
@@ -1,20 +1,21 @@
 var config = require('./config.js');
 var gulp = require('gulp');
 var util = require('gulp-util')
-var usemin = require( 'gulp-usemin')
-var uglify = require( 'gulp-uglify')
+var usemin = require('gulp-usemin')
+var uglify = require('gulp-uglify')
 var htmlmin = require('gulp-htmlmin')
 
 gulp.task('html:dev', function () {
-  return gulp.src(config.paths.src.html)
-    .pipe(gulp.dest(config.paths.builds.dev.root))
+    return gulp.src(config.paths.src.html)
+        .pipe(gulp.dest(config.paths.builds.dev.root))
 });
 
 gulp.task('html:prod', () => {
     return gulp.src(config.paths.src.html)
         .pipe(gulp.dest(config.paths.builds.prod.root))
         .pipe(usemin({
-            vendor: [uglify()],
+            otsimo: [],
+            vendor: [],
             html: [htmlmin({
                 removeComments: true,
                 collapseWhitespace: true
