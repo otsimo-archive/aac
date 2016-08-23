1abddfe88ca4535865bd79e6f971b4cd6e5e3c26
Burak Tokak <iletisim@buraktokak.com>
2016-08-23 14:54:25 +0300

    contains function to check if an array exists

--- app/js/core.js
+++ app/js/core.js
@@ -45,6 +45,33 @@ function deviceType(){
   return type;
 }
 
+var contains = function(needle) {
+    // Per spec, the way to identify NaN is that it is not equal to itself
+    var findNaN = needle !== needle;
+    var indexOf;
+
+    if(!findNaN && typeof Array.prototype.indexOf === 'function') {
+        indexOf = Array.prototype.indexOf;
+    } else {
+        indexOf = function(needle) {
+            var i = -1, index = -1;
+
+            for(i = 0; i < this.length; i++) {
+                var item = this[i];
+
+                if((findNaN && item !== item) || item === needle) {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        };
+    }
+
+    return indexOf.call(this, needle) > -1;
+};
+
 /*
 var statusSettings = 0;
 
