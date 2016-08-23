1578b05d8c08f919e2092230453e3632901dd25e
Burak Tokak <iletisim@buraktokak.com>
2016-08-23 12:53:26 +0300

    keyboard input submit button.

--- app/js/keyboard.js
+++ app/js/keyboard.js
@@ -14,13 +14,26 @@ aacApp.controller('otsControlKeyboard', function ($scope, $global) {
 
   $scope.showKeyboard = function(){
     document.getElementById("typeInput").focus();
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
 
     setTimeout(function(){
       document.body.style.height = parseInt(window.innerHeight)+'px';
       document.body.scrollTop = document.documentElement.scrollTop = 0;
       console.log("virtual keyboard opened!");
-    }, 200);
-  }
+    }, 300);
+  };
+
+  $scope.showKeyboardFocused = function(){
+      setTimeout(function(){
+        document.body.style.height = parseInt(window.innerHeight)+'px';
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+        console.log("virtual keyboard opened!");
+      }, 300);
+    };
+
+  $scope.hideKeyboard = function(){
+      document.body.style.height = "100%";
+  };
 
 $scope.$watch('$viewContentLoaded', function(){
   $scope.showKeyboard();
