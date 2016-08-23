2235baa144516069ae7f569c452a3a5c66c084de
Burak Tokak <iletisim@buraktokak.com>
2016-08-23 09:58:12 +0300

    keyboard template - controller created.

---
+++ app/js/keyboard.js
@@ -0,0 +1,15 @@
+/*
+  -- keyboard.js
+  Directive and controller for the words the user input via keyboard;
+
+*/
+
+aacApp.directive('keyboard', function () {
+    return {
+        templateUrl: 'template/keyboard.html'
+    };
+});
+
+aacApp.controller('otsControlKeyboard', function ($scope, $global) {
+
+});
