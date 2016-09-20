bb4de866b88b279ac891d1312664358d84959a5c
Sercan Degirmenci <degirmencisercan@gmail.com>
2016-09-20 18:31:43 +0300

    fix travis test

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     files: [
       'karma.shim.js',
       'node_modules/karma-read-json/karma-read-json.js',
-      {pattern: 'app/symbols/**/*.json', included: false}
+      { pattern: 'app/symbols/**/*.json', included: false }
     ],
     preprocessors: {
       'karma.shim.js': ['webpack']
@@ -47,19 +47,15 @@ module.exports = function (config) {
   });
   let configuration = {
     // other things
-
     customLaunchers: {
-        Chrome_travis_ci: {
-            base: 'Chrome',
-            flags: ['--no-sandbox']
-        }
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
     },
   };
-
-    if (process.env.TRAVIS) {
-        configuration.browsers = ['Chrome_travis_ci'];
-    }
-
-    config.set(configuration);
-
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+  config.set(configuration);
 }
