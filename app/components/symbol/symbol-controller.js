53db3dfbdeecfc51595ebad0a45b3acb7253523d
Burak Tokak <iletisim@buraktokak.com>
2016-10-10 11:59:04 +0300

    speak when conjuncted clicked

--- app/components/symbol/symbol-controller.js
+++ app/components/symbol/symbol-controller.js
@@ -9,149 +9,150 @@ import * as CONSTANT from '../../js/constants';
  * @class SymbolController
  */
 export default class SymbolController {
-  /**
-   * Creates an instance of SymbolController.
-   *
-   * @param {any} $scope
-   * @param {any} $http
-   * @param {any} $timeout
-   * @param {any} $global
-   * @param {EventManager} EventManager
-   * @param {TTSManager} TTSManager
-   *
-   * @memberOf SymbolController
-   */
-  constructor($scope, $http, $timeout, $global, EventManager, TTSManager) {
-    this.$scope = $scope;
-    this.$scope.global = $global;
-    this.$timeout = $timeout;
-    this.events = EventManager;
-    this.tts = TTSManager;
+	/**
+	 * Creates an instance of SymbolController.
+	 *
+	 * @param {any} $scope
+	 * @param {any} $http
+	 * @param {any} $timeout
+	 * @param {any} $global
+	 * @param {EventManager} EventManager
+	 * @param {TTSManager} TTSManager
+	 *
+	 * @memberOf SymbolController
+	 */
+	constructor($scope, $http, $timeout, $global, EventManager, TTSManager) {
+		this.$scope = $scope;
+		this.$scope.global = $global;
+		this.$timeout = $timeout;
+		this.events = EventManager;
+		this.tts = TTSManager;
 
-    // Initilize variables for controller.
-    this.wordTouchTimer = null;
-    this.currentlyHolding = null;
-  }
+		// Initilize variables for controller.
+		this.wordTouchTimer = null;
+		this.currentlyHolding = null;
+	}
 
-  /**
-   * Called when user starts touch action.
-   * Handles hold actions if user holds on symbol 300ms
-   * @param {Object} wordObj
-   * @param {number} index
-   */
-  wordTouchStart(wordObj, index) {
-    this.currentlyHolding = wordObj.title;
-    this.wordTouchTimer = this.$timeout(() => {
-      if (wordObj.class === CONSTANT.CLASS_DERIVABLE) {
-        this.$scope.global.currentDerivable = wordObj.slug;
-        this.events.appDerive(wordObj.slug);
-        this.$scope.global.changeCurrentTab(CONSTANT.TAB_DERIVABLE);
-      } else if (wordObj.class === CONSTANT.CLASS_GROUP) {
-        this.$scope.global.currentGroup = wordObj.slug;
-        this.$scope.global.changeCurrentTab(CONSTANT.TAB_GROUP);
-        /*
-        Category animation killing in touchEnd:
-        */
-      } else if (wordObj.type === "verb") {
-        this.openVerbConjunction(wordObj);
-      }
-      this.clickAnimStop();
-    }, 300);
-    this.clickAnimStart(index);
-  }
+	/**
+	 * Called when user starts touch action.
+	 * Handles hold actions if user holds on symbol 300ms
+	 * @param {Object} wordObj
+	 * @param {number} index
+	 */
+	wordTouchStart(wordObj, index) {
+		this.currentlyHolding = wordObj.title;
+		this.wordTouchTimer = this.$timeout(() => {
+			if (wordObj.class === CONSTANT.CLASS_DERIVABLE) {
+				this.$scope.global.currentDerivable = wordObj.slug;
+				this.events.appDerive(wordObj.slug);
+				this.$scope.global.changeCurrentTab(CONSTANT.TAB_DERIVABLE);
+			} else if (wordObj.class === CONSTANT.CLASS_GROUP) {
+				this.$scope.global.currentGroup = wordObj.slug;
+				this.$scope.global.changeCurrentTab(CONSTANT.TAB_GROUP);
+				/*
+				Category animation killing in touchEnd:
+				*/
+			} else if (wordObj.type === "verb") {
+				this.openVerbConjunction(wordObj);
+			}
+			this.clickAnimStop();
+		}, 300);
+		this.clickAnimStart(index);
+	}
 
-  openVerbConjunction(wordObj) {
-    this.conjArr = [];
-    CONSTANT.CONJTYPE["tr"].forEach(c => {
-      let conjable = {
-        title: turkishConjunctor(wordObj.title, c),
-        tence: c
-      };
-      this.conjArr.push(conjable);
-    });
-    document.getElementById("verbConj").style.display = "block";
-  }
+	openVerbConjunction(wordObj) {
+		this.conjArr = [];
+		CONSTANT.CONJTYPE["tr"].forEach(c => {
+			let conjable = {
+				title: turkishConjunctor(wordObj.title, c),
+				tence: c
+			};
+			this.conjArr.push(conjable);
+		});
+		document.getElementById("verbConj").style.display = "block";
+	}
 
-  clickConjuncted(wordTitle, tence) {
-    let wordObj = {};
-    wordObj.title = wordTitle;
-    wordObj.slug = this.$scope.global.extendedSlugMap[wordTitle];
-    wordObj.type = "verb";
-    wordObj.tence = tence;
-    this.$scope.global.pushToCurrentPhrase(wordObj);
-    document.getElementById("verbConj").style.display = "none";
-  }
+	clickConjuncted(wordTitle, tence) {
+		let wordObj = {};
+		wordObj.title = wordTitle;
+		wordObj.slug = this.$scope.global.extendedSlugMap[wordTitle];
+		wordObj.type = "verb";
+		wordObj.tence = tence;
+		this.$scope.global.pushToCurrentPhrase(wordObj);
+		this.tts.speak(wordObj.title);
+		document.getElementById("verbConj").style.display = "none";
+	}
 
-  /**
-   * Called when user ends touch event.
-   * Cancels the onhold actions when touch canceled before 300ms
-   * @param {Object} wordObj
-   * @param {number} index
-   */
-  wordTouchEnd(wordObj, index) {
-    this.$timeout.cancel(this.wordTouchTimer);
-    if (this.currentlyHolding === wordObj.title && wordObj.class !== CONSTANT.CLASS_GROUP) {
-      if (wordObj.type == "verb") {
-        if (this.wordTouchTimer.$$state.status == 2) {
-          this.clickWord(wordObj);
-        }
-      } else {
-        this.clickWord(wordObj);
-      }
-    }
-    if (wordObj.class === 'group') {
-      this.$scope.global.currentGroup = wordObj.slug;
-      this.$scope.global.changeCurrentTab(CONSTANT.TAB_GROUP);
-    }
-    this.clickAnimEnd(index);
-  }
+	/**
+	 * Called when user ends touch event.
+	 * Cancels the onhold actions when touch canceled before 300ms
+	 * @param {Object} wordObj
+	 * @param {number} index
+	 */
+	wordTouchEnd(wordObj, index) {
+		this.$timeout.cancel(this.wordTouchTimer);
+		if (this.currentlyHolding === wordObj.title && wordObj.class !== CONSTANT.CLASS_GROUP) {
+			if (wordObj.type == "verb") {
+				if (this.wordTouchTimer.$$state.status == 2) {
+					this.clickWord(wordObj);
+				}
+			} else {
+				this.clickWord(wordObj);
+			}
+		}
+		if (wordObj.class === 'group') {
+			this.$scope.global.currentGroup = wordObj.slug;
+			this.$scope.global.changeCurrentTab(CONSTANT.TAB_GROUP);
+		}
+		this.clickAnimEnd(index);
+	}
 
-  /**
-   * Starts click animation by adding className to the symbol with given index
-   * @param {number} index
-   */
-  clickAnimStart(index) {
-    let elem = document.getElementById('item-' + index);
-    if (elem) {
-      elem.className = elem.className + ' gridItemClick';
-    }
-    this.$scope.currentAnimIndex = index;
-  }
+	/**
+	 * Starts click animation by adding className to the symbol with given index
+	 * @param {number} index
+	 */
+	clickAnimStart(index) {
+		let elem = document.getElementById('item-' + index);
+		if (elem) {
+			elem.className = elem.className + ' gridItemClick';
+		}
+		this.$scope.currentAnimIndex = index;
+	}
 
-  /**
-   * Reverses click animation by removing className to the symbol with given index
-   * @param {number} index
-   */
-  clickAnimEnd(index) {
-    let elem = document.getElementById('item-' + index);
-    if (elem) {
-      elem.className = elem.className.replace(' gridItemClick', '');
-    }
-  }
+	/**
+	 * Reverses click animation by removing className to the symbol with given index
+	 * @param {number} index
+	 */
+	clickAnimEnd(index) {
+		let elem = document.getElementById('item-' + index);
+		if (elem) {
+			elem.className = elem.className.replace(' gridItemClick', '');
+		}
+	}
 
-  /**
-   * Cancels the animation for all symbol grids.
-   * Kills transition by adding a className (transitionKill)
-   */
-  clickAnimStop() {
-    let elem = document.getElementById('item-' + this.$scope.currentAnimIndex);
-    if (elem) {
-      elem.className = elem.className.replace(' gridItemClick', '') + ' transitionKill';
-      this.$timeout(() => elem.className = elem.className.replace(' transitionKill', ''), 101);
-    }
-  }
+	/**
+	 * Cancels the animation for all symbol grids.
+	 * Kills transition by adding a className (transitionKill)
+	 */
+	clickAnimStop() {
+		let elem = document.getElementById('item-' + this.$scope.currentAnimIndex);
+		if (elem) {
+			elem.className = elem.className.replace(' gridItemClick', '') + ' transitionKill';
+			this.$timeout(() => elem.className = elem.className.replace(' transitionKill', ''), 101);
+		}
+	}
 
-  /**
-   * Pushes the symbol object to current phrase
-   * Sends word's string to device's TTS Service.
-   * @param {Object} wordObj
-   */
-  clickWord(wordObj) {
-    this.$scope.global.pushToCurrentPhrase(wordObj);
-    updateCurrentPhraseScroll();
-    this.tts.speak(wordObj.title);
-    this.events.appWord(wordObj.title, this.$scope.global.gridSize[0], this.$scope.global.gridSize[1]);
-  }
+	/**
+	 * Pushes the symbol object to current phrase
+	 * Sends word's string to device's TTS Service.
+	 * @param {Object} wordObj
+	 */
+	clickWord(wordObj) {
+		this.$scope.global.pushToCurrentPhrase(wordObj);
+		updateCurrentPhraseScroll();
+		this.tts.speak(wordObj.title);
+		this.events.appWord(wordObj.title, this.$scope.global.gridSize[0], this.$scope.global.gridSize[1]);
+	}
 }
 
 // Service Dependency Injection
