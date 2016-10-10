630823f2d0d459ed462702df9782455fb5b097f5
Burak Tokak <iletisim@buraktokak.com>
2016-10-10 14:45:34 +0300

    Move prototype functions to init.js

--- app/js/init.js
+++ app/js/init.js
@@ -1,22 +1,30 @@
-document.ontouchmove = function (event) {
-  event.preventDefault();
+document.ontouchmove = function(event) {
+	event.preventDefault();
 }
 
 if ('addEventListener' in document) {
-  document.addEventListener('DOMContentLoaded', function () {
-    FastClick.attach(document.body);
-  }, false);
+	document.addEventListener('DOMContentLoaded', function() {
+		FastClick.attach(document.body);
+	}, false);
 }
 
 if (!localStorage.phraseHistory) {
-  console.log('LS: not yet initilized, firstTime load.');
-  localStorage.phraseHistory = '[]';
+	console.log('LS: not yet initilized, firstTime load.');
+	localStorage.phraseHistory = '[]';
 }
 
-Array.prototype.contains = function (obj) {
-  return this.indexOf(obj) > -1;
+Array.prototype.contains = function(obj) {
+	return this.indexOf(obj) > -1;
 };
 
-String.prototype.contains = function (it) {
-  return this.indexOf(it) != -1;
+String.prototype.contains = function(it) {
+	return this.indexOf(it) != -1;
+};
+
+String.prototype.replaceAll = function(search, replace) {
+	if (replace === undefined) {
+		return this.toString();
+	}
+
+	return this.replace(new RegExp('[' + search + ']', 'g'), replace);
 };
