208aa4db70899ba2434a0ddb1074b70df34009a9
Burak Tokak <iletisim@buraktokak.com>
2016-10-10 11:54:17 +0300

    speak when recognized word is clicked

--- app/app-controller.js
+++ app/app-controller.js
@@ -7,207 +7,206 @@ import * as CONSTANT from './js/constants';
  * @class AppController
  */
 export default class AppController {
-  /**
-   * Creates an instance of AppController.
-   *
-   * @param {any} $scope
-   * @param {any} $global
-   * @param {angular.IHttpService} $http
-   * @param {TTSManager} TTSManager
-   * @param {any} OtsimoHandler
-   *
-   * @memberOf AppController
-   */
-  constructor($scope, $global, $http, TTSManager, OtsimoHandler) {
-    this.$scope = $scope;
-    this.$scope.global = $global;
-    this.$http = $http;
-    this.tts = TTSManager;
-    this.otsimo = OtsimoHandler.init();
-    // Initilize variables for controller.
+	/**
+	 * Creates an instance of AppController.
+	 *
+	 * @param {any} $scope
+	 * @param {any} $global
+	 * @param {angular.IHttpService} $http
+	 * @param {TTSManager} TTSManager
+	 * @param {any} OtsimoHandler
+	 *
+	 * @memberOf AppController
+	 */
+	constructor($scope, $global, $http, TTSManager, OtsimoHandler) {
+		this.$scope = $scope;
+		this.$scope.global = $global;
+		this.$http = $http;
+		this.tts = TTSManager;
+		this.otsimo = OtsimoHandler.init();
+		// Initilize variables for controller.
 
-    // Call controllerInit
-    this.controllerInit();
-  }
+		// Call controllerInit
+		this.controllerInit();
+	}
 
-  controllerInit() {
-    // Create or facilitate new functions for $scope or $global service.
-    this.otsimo.run(() => {
+	controllerInit() {
+		// Create or facilitate new functions for $scope or $global service.
+		this.otsimo.run(() => {
 
-      this.checkCapabilities();
-      this.setSettings();
-      this.runApp();
-    });
+			this.checkCapabilities();
+			this.setSettings();
+			this.runApp();
+		});
 
-    // Listen the changes on the resolution and
-    // orientation from the device.
-    this.resolutionListener();
-  }
+		// Listen the changes on the resolution and
+		// orientation from the device.
+		this.resolutionListener();
+	}
 
-  /**
-   * Does the inital tasks to start the app.
-   */
-  runApp() {
-    // Get grid size X and Y from otsimo settings object.
-    let otsGridSize = this.otsimo.settings.gridsize.split('grid-')[1];
-    let otsGridXY = otsGridSize.split('x');
-    let x = otsGridXY[0];
-    let y = otsGridXY[1];
-    // Check if the device is a phone
-    // Limit the grid size to 6x4
-    if (deviceType() === 'phone') {
-      if (x > 6) { x = 6; }
-      if (y > 4) { y = 4; }
-    }
-    // Set grid size and check device orientation.
-    this.$scope.global.changeGridSize(x, y);
-    if (this.otsimo.width < this.otsimo.height) {
-      this.$scope.global.checkOrientation(CONSTANT.PORTRAIT);
-    } else {
-      this.$scope.global.checkOrientation(CONSTANT.LANDSCAPE_LEFT);
-    }
+	/**
+	 * Does the inital tasks to start the app.
+	 */
+	runApp() {
+		// Get grid size X and Y from otsimo settings object.
+		let otsGridSize = this.otsimo.settings.gridsize.split('grid-')[1];
+		let otsGridXY = otsGridSize.split('x');
+		let x = otsGridXY[0];
+		let y = otsGridXY[1];
+		// Check if the device is a phone
+		// Limit the grid size to 6x4
+		if (deviceType() === 'phone') {
+			if (x > 6) { x = 6; }
+			if (y > 4) { y = 4; }
+		}
+		// Set grid size and check device orientation.
+		this.$scope.global.changeGridSize(x, y);
+		if (this.otsimo.width < this.otsimo.height) {
+			this.$scope.global.checkOrientation(CONSTANT.PORTRAIT);
+		} else {
+			this.$scope.global.checkOrientation(CONSTANT.LANDSCAPE_LEFT);
+		}
 
-    // Load symbol data to global array variable.
-    this.loadSymbols();
-  }
+		// Load symbol data to global array variable.
+		this.loadSymbols();
+	}
 
-  /**
-   * Sets the setting properties fron otsimo object
-   *
-   */
-  setSettings() {
+	/**
+	 * Sets the setting properties fron otsimo object
+	 *
+	 */
+	setSettings() {
 
-    this.$scope.global.language = this.otsimo.child.language;
-    // Colors & styles
-    this.$scope.headerColor = this.otsimo.kv.headerColor;
-    this.$scope.generalFont = this.otsimo.kv.generalFont;
-    let header = document.getElementById('header');
-    if (header) {
-      header.style.background = this.$scope.headerColor;
-      document.body.style.fontSize = this.$scope.generalFont;
-    }
+		this.$scope.global.language = this.otsimo.child.language;
+		// Colors & styles
+		this.$scope.headerColor = this.otsimo.kv.headerColor;
+		this.$scope.generalFont = this.otsimo.kv.generalFont;
+		let header = document.getElementById('header');
+		if (header) {
+			header.style.background = this.$scope.headerColor;
+			document.body.style.fontSize = this.$scope.generalFont;
+		}
 
-    this.setUIText();
+		this.setUIText();
 
-  }
+	}
 
-  /**
-   * Loads symbol package respect to language
-   *
-   */
-  loadSymbols() {
-    let global = this.$scope.global;
-    let metadataPath = `${this.otsimo.kv.symbolPack}/metadata.json`;
-    this.$http.get(metadataPath)
-      .success((resp) => {
-        this.metadata = resp;
-        global.symbolPath = `${this.otsimo.kv.symbolPack}/${resp.images}`;
-        this.tts.setVoiceDriver(resp.voiceId);
+	/**
+	 * Loads symbol package respect to language
+	 *
+	 */
+	loadSymbols() {
+		let global = this.$scope.global;
+		let metadataPath = `${this.otsimo.kv.symbolPack}/metadata.json`;
+		this.$http.get(metadataPath)
+			.success((resp) => {
+				this.metadata = resp;
+				global.symbolPath = `${this.otsimo.kv.symbolPack}/${resp.images}`;
+				this.tts.setVoiceDriver(resp.voiceId);
 
-        let symbolDataPath = `${this.otsimo.kv.symbolPack}/${resp.data}`
+				let symbolDataPath = `${this.otsimo.kv.symbolPack}/${resp.data}`
 
-        this.$http.get(symbolDataPath)
-          .success(resp => {
-            global.mainArray = resp.symbols;
-            this.initExtendedSymbols();
-            global.changeCurrentTab(CONSTANT.TAB_MAIN);
-          })
-          .error(console.error);
-      })
-      .error(console.error);
-  }
+				this.$http.get(symbolDataPath)
+					.success(resp => {
+						global.mainArray = resp.symbols;
+						this.initExtendedSymbols();
+						global.changeCurrentTab(CONSTANT.TAB_MAIN);
+					})
+					.error(console.error);
+			})
+			.error(console.error);
+	}
 
-  /**
-   * Initilizes the extended symbol array for phrase view.
-   *
-   */
-  initExtendedSymbols() {
-    let global = this.$scope.global;
-    if (global.mainArray) {
-      global.mainArray.forEach(objMirror => {
-        let obj = JSON.parse(JSON.stringify(objMirror));
-        global.extendedArray.push(obj);
-        global.extendedSlugMap[obj.title] = obj.slug;
-        // Extend synonyms
-        if (obj.synonym.length) {
-          obj.synonym.forEach(s => {
-            obj.title = s;
-            let pushObj = JSON.parse(JSON.stringify(obj));
-            global.extendedArray.push(pushObj);
-            global.extendedSlugMap[pushObj.title] = obj.slug;
-          });
-        }
-        // Extend conjuncted
-        if (obj.type == "verb") {
-          let lang = this.$scope.global.language;
-          if (lang == "tr") {
-            let possessors = ["ben", "sen", "o"];
-            possessors.forEach(p => {
-              CONSTANT.CONJTYPE["tr"].forEach(c => {
-                obj.title = turkishConjunctor(obj.slug, c, p);
-                console.log(obj);
-                let pushObj = JSON.parse(JSON.stringify(obj));
-                global.extendedArray.push(pushObj);
-                global.extendedSlugMap[pushObj.title] = obj.slug;
-              });
-            });
-          } else if (lang == "en") {
-            //Set english verb conjunction.
-          }
-        }
-      });
-    }
-    global.extendedArray.forEach(ext => {
-      global.extendedTitleArray.push(ext.title);
-      global.extendedSlugArray.push(ext.slug);
-    });
-  }
+	/**
+	 * Initilizes the extended symbol array for phrase view.
+	 *
+	 */
+	initExtendedSymbols() {
+		let global = this.$scope.global;
+		if (global.mainArray) {
+			global.mainArray.forEach(objMirror => {
+				let obj = JSON.parse(JSON.stringify(objMirror));
+				global.extendedArray.push(obj);
+				global.extendedSlugMap[obj.title] = obj.slug;
+				// Extend synonyms
+				if (obj.synonym.length) {
+					obj.synonym.forEach(s => {
+						obj.title = s;
+						let pushObj = JSON.parse(JSON.stringify(obj));
+						global.extendedArray.push(pushObj);
+						global.extendedSlugMap[pushObj.title] = obj.slug;
+					});
+				}
+				// Extend conjuncted
+				if (obj.type == "verb") {
+					let lang = this.$scope.global.language;
+					if (lang == "tr") {
+						let possessors = ["ben", "sen", "o"];
+						possessors.forEach(p => {
+							CONSTANT.CONJTYPE["tr"].forEach(c => {
+								obj.title = turkishConjunctor(obj.slug, c, p);
+								let pushObj = JSON.parse(JSON.stringify(obj));
+								global.extendedArray.push(pushObj);
+								global.extendedSlugMap[pushObj.title] = obj.slug;
+							});
+						});
+					} else if (lang == "en") {
+						//Set english verb conjunction.
+					}
+				}
+			});
+		}
+		global.extendedArray.forEach(ext => {
+			global.extendedTitleArray.push(ext.title);
+			global.extendedSlugArray.push(ext.slug);
+		});
+	}
 
-  /**
-   * Checks if the device or installed otsimo app is supporting TTS.
-   *
-   */
-  checkCapabilities() {
-    if (this.otsimo.capabilities.indexOf('tts') === -1) {
-      document.getElementById('outdated').style.display = 'block';
-    }
-  }
+	/**
+	 * Checks if the device or installed otsimo app is supporting TTS.
+	 *
+	 */
+	checkCapabilities() {
+		if (this.otsimo.capabilities.indexOf('tts') === -1) {
+			document.getElementById('outdated').style.display = 'block';
+		}
+	}
 
-  /**
-   * Sets UI text setting properties from otsimo kv object
-   *
-   */
-  setUIText() {
-    this.$scope.pageText1 = this.otsimo.kv.pageText1;
-    this.$scope.pageText2 = this.otsimo.kv.pageText2;
-    this.$scope.pageText3 = this.otsimo.kv.pageText3;
-    this.$scope.pageText4 = this.otsimo.kv.pageText4;
-    this.$scope.pageText5 = this.otsimo.kv.pageText5;
-    this.$scope.timeIntervalText1 = this.otsimo.kv.timeIntervalText1;
-    this.$scope.timeIntervalText2 = this.otsimo.kv.timeIntervalText2;
-    this.$scope.timeIntervalText3 = this.otsimo.kv.timeIntervalText3;
-    this.$scope.timeIntervalText4 = this.otsimo.kv.timeIntervalText4;
-    this.$scope.previousText = this.otsimo.kv.previousText;
-    this.$scope.nextText = this.otsimo.kv.nextText;
-    this.$scope.backText = this.otsimo.kv.backText;
-    this.$scope.removeHoldColor = this.otsimo.kv.removeHoldColor;
-    this.$scope.removeNormalColor = this.otsimo.kv.removeNormalColor;
-    this.$scope.startTyping = this.otsimo.kv.startTyping;
-  }
+	/**
+	 * Sets UI text setting properties from otsimo kv object
+	 *
+	 */
+	setUIText() {
+		this.$scope.pageText1 = this.otsimo.kv.pageText1;
+		this.$scope.pageText2 = this.otsimo.kv.pageText2;
+		this.$scope.pageText3 = this.otsimo.kv.pageText3;
+		this.$scope.pageText4 = this.otsimo.kv.pageText4;
+		this.$scope.pageText5 = this.otsimo.kv.pageText5;
+		this.$scope.timeIntervalText1 = this.otsimo.kv.timeIntervalText1;
+		this.$scope.timeIntervalText2 = this.otsimo.kv.timeIntervalText2;
+		this.$scope.timeIntervalText3 = this.otsimo.kv.timeIntervalText3;
+		this.$scope.timeIntervalText4 = this.otsimo.kv.timeIntervalText4;
+		this.$scope.previousText = this.otsimo.kv.previousText;
+		this.$scope.nextText = this.otsimo.kv.nextText;
+		this.$scope.backText = this.otsimo.kv.backText;
+		this.$scope.removeHoldColor = this.otsimo.kv.removeHoldColor;
+		this.$scope.removeNormalColor = this.otsimo.kv.removeNormalColor;
+		this.$scope.startTyping = this.otsimo.kv.startTyping;
+	}
 
-  /**
-   * Listens any resolution changes
-   *
-   */
-  resolutionListener() {
-    this.otsimo.onResolutionChanged((width, height, orientation) => {
-      if (width < height) {
-        this.$scope.global.checkOrientation(CONSTANT.PORTRAIT);
-      } else {
-        this.$scope.global.checkOrientation(CONSTANT.LANDSCAPE_LEFT);
-      }
-    });
-  }
+	/**
+	 * Listens any resolution changes
+	 *
+	 */
+	resolutionListener() {
+		this.otsimo.onResolutionChanged((width, height, orientation) => {
+			if (width < height) {
+				this.$scope.global.checkOrientation(CONSTANT.PORTRAIT);
+			} else {
+				this.$scope.global.checkOrientation(CONSTANT.LANDSCAPE_LEFT);
+			}
+		});
+	}
 
 }
 // Service Dependency Injection
--- app/components/keyboard/keyboard-controller.js
+++ app/components/keyboard/keyboard-controller.js
@@ -6,231 +6,231 @@ import { returnTime, updateCurrentPhraseScroll } from 'js/utils';
  * @class KeyboardController
  */
 export default class KeyboardController {
-  /**
-   * Creates an instance of KeyboardController.
-   *
-   * @param {any} $scope
-   * @param {any} $global
-   * @param {any} $timeout
-   * @param {TTSManager} TTSManager
-   *
-   * @memberOf RecentController
-   */
-  constructor($scope, $global, $timeout, EventManager, TTSManager) {
-    this.$scope = $scope;
-    this.$scope.global = $global;
-    this.$timeout = $timeout;
-    this.tts = TTSManager;
-    this.event = EventManager;
-    this.fastTypeTimer = null;
-    this.suggestions = false;
-    // Initilize variables for controller.
+	/**
+	 * Creates an instance of KeyboardController.
+	 *
+	 * @param {any} $scope
+	 * @param {any} $global
+	 * @param {any} $timeout
+	 * @param {TTSManager} TTSManager
+	 *
+	 * @memberOf RecentController
+	 */
+	constructor($scope, $global, $timeout, EventManager, TTSManager) {
+		this.$scope = $scope;
+		this.$scope.global = $global;
+		this.$timeout = $timeout;
+		this.tts = TTSManager;
+		this.event = EventManager;
+		this.fastTypeTimer = null;
+		this.suggestions = false;
+		// Initilize variables for controller.
 
-    // Call controllerInit
-    this.controllerInit();
-  }
+		// Call controllerInit
+		this.controllerInit();
+	}
 
-  /**
-   * Create or facilitate new functions for $scope or $global service.
-   */
-  controllerInit() {
-    this.phraseIndex = 0;
-    this.suggestionList = [];
-  }
+	/**
+	 * Create or facilitate new functions for $scope or $global service.
+	 */
+	controllerInit() {
+		this.phraseIndex = 0;
+		this.suggestionList = [];
+	}
 
-  /**
-   * Shows the keyboard
-   */
-  showKeyboard() {
-    let typeInput = document.getElementById("typeInput");
-    if (typeInput) {
-      typeInput.focus();
-      document.body.scrollTop = document.documentElement.scrollTop = 0;
-    }
-    this.showKeyboardFocused();
-  }
+	/**
+	 * Shows the keyboard
+	 */
+	showKeyboard() {
+		let typeInput = document.getElementById("typeInput");
+		if (typeInput) {
+			typeInput.focus();
+			document.body.scrollTop = document.documentElement.scrollTop = 0;
+		}
+		this.showKeyboardFocused();
+	}
 
-  /**
-   * Removes readonly from typeInput
-   * Triggered when type input is
-   * Walkaround for ios autocorrect
-   */
-  showKeyboardFocused() {
-    let typeInput = document.getElementById("typeInput");
-    if (typeInput) {
-      document.body.style.height = (parseInt(window.innerHeight) / 2) + 'px';
-      document.body.scrollTop = document.documentElement.scrollTop = 0;
-      this.$timeout(() => {
-        document.body.style.height = parseInt(window.innerHeight) + 'px';
-        document.body.scrollTop = document.documentElement.scrollTop = 0;
-        console.log("virtual keyboard opened!");
-      }, 300);
-    }
-  }
+	/**
+	 * Removes readonly from typeInput
+	 * Triggered when type input is
+	 * Walkaround for ios autocorrect
+	 */
+	showKeyboardFocused() {
+		let typeInput = document.getElementById("typeInput");
+		if (typeInput) {
+			document.body.style.height = (parseInt(window.innerHeight) / 2) + 'px';
+			document.body.scrollTop = document.documentElement.scrollTop = 0;
+			this.$timeout(() => {
+				document.body.style.height = parseInt(window.innerHeight) + 'px';
+				document.body.scrollTop = document.documentElement.scrollTop = 0;
+				console.log("virtual keyboard opened!");
+			}, 300);
+		}
+	}
 
-  /**
-   * Hides the keyboard
-   */
-  hideKeyboard() {
-    let typeInput = document.getElementById("typeInput");
-    if (typeInput) {
-      document.body.style.height = "100%";
-    }
-  }
+	/**
+	 * Hides the keyboard
+	 */
+	hideKeyboard() {
+		let typeInput = document.getElementById("typeInput");
+		if (typeInput) {
+			document.body.style.height = "100%";
+		}
+	}
 
-  /**
-   * Submits (handles) the current typeInput string content.
-   */
-  submitCurrentInput() {
-    let typeInput = document.getElementById("typeInput");
-    if (typeInput.value) {
-      let inputWord = {};
-      inputWord.title = typeInput.value.toLocaleLowerCase();
-      inputWord.slug = this.$scope.global.extendedSlugMap[inputWord.title];
-      let checkExist = this.checkWordInDB(inputWord.slug);
-      inputWord.slugExist = !checkExist;
+	/**
+	 * Submits (handles) the current typeInput string content.
+	 */
+	submitCurrentInput() {
+		let typeInput = document.getElementById("typeInput");
+		if (typeInput.value) {
+			let inputWord = {};
+			inputWord.title = typeInput.value.toLocaleLowerCase();
+			inputWord.slug = this.$scope.global.extendedSlugMap[inputWord.title];
+			let checkExist = this.checkWordInDB(inputWord.slug);
+			inputWord.slugExist = !checkExist;
 
-      if (inputWord.title.contains(" ") && !checkExist) {
-        this.recognizeWord(inputWord.title);
-      } else {
-        inputWord.phraseIndex = this.phraseIndex++;
-        this.$scope.global.pushToCurrentPhrase(inputWord);
-      }
-      updateCurrentPhraseScroll();
-      this.tts.speak(inputWord.title);
-      let gs = this.$scope.global.gridSize;
-      this.event.appWord(inputWord.title, gs[0], gs[1]);
-      typeInput.value = "";
-    }
-  }
+			if (inputWord.title.contains(" ") && !checkExist) {
+				this.recognizeWord(inputWord.title);
+			} else {
+				inputWord.phraseIndex = this.phraseIndex++;
+				this.$scope.global.pushToCurrentPhrase(inputWord);
+			}
+			updateCurrentPhraseScroll();
+			this.tts.speak(inputWord.title);
+			let gs = this.$scope.global.gridSize;
+			this.event.appWord(inputWord.title, gs[0], gs[1]);
+			typeInput.value = "";
+		}
+	}
 
-  /**
-   * Handles the submission of the input via "enter" from the device
-   */
-  enterSubmit($event) {
-    if ($event.keyCode === 13) {
-      this.submitCurrentInput();
-      this.suggestionList = [];
-    } else {
-      let typeInput = document.getElementById("typeInput");
-      if (typeInput) {
-        // If there is a timeout, clear it.
-        if (this.fastTypeTimer) {
-          this.$timeout.cancel(this.fastTypeTimer);
-        }
-        this.fastTypeTimer = this.$timeout(() => {
-          if (typeInput.value.length > 1) {
-            this.suggestWordsByInput(typeInput.value);
-          } else {
-            this.suggestionList = [];
-            this.suggestions = false;
-          }
-        }, 300);
-      }
-    }
-  }
+	/**
+	 * Handles the submission of the input via "enter" from the device
+	 */
+	enterSubmit($event) {
+		if ($event.keyCode === 13) {
+			this.submitCurrentInput();
+			this.suggestionList = [];
+		} else {
+			let typeInput = document.getElementById("typeInput");
+			if (typeInput) {
+				// If there is a timeout, clear it.
+				if (this.fastTypeTimer) {
+					this.$timeout.cancel(this.fastTypeTimer);
+				}
+				this.fastTypeTimer = this.$timeout(() => {
+					if (typeInput.value.length > 1) {
+						this.suggestWordsByInput(typeInput.value);
+					} else {
+						this.suggestionList = [];
+						this.suggestions = false;
+					}
+				}, 300);
+			}
+		}
+	}
 
-  /**
-   * Handles, creates the data to set suggestions by,
-   * current keyboard input.
-   */
-  suggestWordsByInput(searchLetter) {
-    this.suggestions = true;
-    searchLetter = searchLetter.toLocaleLowerCase();
-    searchLetter = searchLetter.replace(" ", "-");
-    this.suggestionList = this.$scope.global.extendedTitleArray.filter((word) => {
-      return word.substring(0, searchLetter.length) == searchLetter;
-    });
-    if (this.suggestionList.length > 0) {
-      this.suggestionList = this.suggestionList.filter((it, i, ar) => ar.indexOf(it) === i);
-      this.suggestionList.sort(this.sortByLength);
-    }
-  }
+	/**
+	 * Handles, creates the data to set suggestions by,
+	 * current keyboard input.
+	 */
+	suggestWordsByInput(searchLetter) {
+		this.suggestions = true;
+		searchLetter = searchLetter.toLocaleLowerCase();
+		searchLetter = searchLetter.replace(" ", "-");
+		this.suggestionList = this.$scope.global.extendedTitleArray.filter((word) => {
+			return word.substring(0, searchLetter.length) == searchLetter;
+		});
+		if (this.suggestionList.length > 0) {
+			this.suggestionList = this.suggestionList.filter((it, i, ar) => ar.indexOf(it) === i);
+			this.suggestionList.sort(this.sortByLength);
+		}
+	}
 
-  /**
-   * Handles actions when a suggestion symbol is clicked
-   */
-  clickSuggestion(wordSlug) {
-    let wordObj2Push = {};
-    wordObj2Push.title = wordSlug.replace("-", " ");
-    wordObj2Push.slug = this.$scope.global.extendedSlugMap[wordSlug];
-    if (this.isVerb(wordSlug)) {
-      wordObj2Push.type = "verb";
-    }
-    wordObj2Push.phraseIndex = this.phraseIndex++;
-    this.$scope.global.pushToCurrentPhrase(wordObj2Push);
+	/**
+	 * Handles actions when a suggestion symbol is clicked
+	 */
+	clickSuggestion(wordSlug) {
+		let wordObj2Push = {};
+		wordObj2Push.title = wordSlug.replace("-", " ");
+		wordObj2Push.slug = this.$scope.global.extendedSlugMap[wordSlug];
+		if (this.isVerb(wordSlug)) {
+			wordObj2Push.type = "verb";
+		}
+		wordObj2Push.phraseIndex = this.phraseIndex++;
+		this.$scope.global.pushToCurrentPhrase(wordObj2Push);
+		this.tts.speak(wordObj2Push.title);
+		let typeInput = document.getElementById("typeInput");
+		if (typeInput) {
+			document.getElementById("typeInput").value = "";
+		}
+		this.suggestionList = [];
+		this.suggestions = false;
+	}
 
-    let typeInput = document.getElementById("typeInput");
-    if (typeInput) {
-      document.getElementById("typeInput").value = "";
-    }
-    this.suggestionList = [];
-    this.suggestions = false;
-  }
+	/**
+	 * Recognizes the string given by the current word package before pushing.
+	 * Then pushes the pieces indivicually.
+	 */
+	recognizeWord(word) {
+		// this function can be better algorithmicly!
+		let splittedUnrecognizedWord = word.split(" ");
+		splittedUnrecognizedWord.forEach((wordPiece) => {
+			if (wordPiece) {
+				let wordObj2Push = {};
+				wordObj2Push.title = wordPiece;
+				wordObj2Push.slug = this.$scope.global.extendedSlugMap[wordPiece];
+				wordObj2Push.slugExist = !this.checkWordInDB(wordObj2Push.slug);
+				wordObj2Push.phraseIndex = this.phraseIndex++;
+				this.$scope.global.pushToCurrentPhrase(wordObj2Push);
+			}
+		});
+	}
 
-  /**
-   * Recognizes the string given by the current word package before pushing.
-   * Then pushes the pieces indivicually.
-   */
-  recognizeWord(word) {
-    // this function can be better algorithmicly!
-    let splittedUnrecognizedWord = word.split(" ");
-    splittedUnrecognizedWord.forEach((wordPiece) => {
-      if (wordPiece) {
-        let wordObj2Push = {};
-        wordObj2Push.title = wordPiece;
-        wordObj2Push.slug = this.$scope.global.extendedSlugMap[wordPiece];
-        wordObj2Push.slugExist = !this.checkWordInDB(wordObj2Push.slug);
-        wordObj2Push.phraseIndex = this.phraseIndex++;
-        this.$scope.global.pushToCurrentPhrase(wordObj2Push);
-      }
-    });
-  }
+	/**
+	 * Checks if the given word in the mainSlugArray
+	 */
+	checkWordInDB(word) {
+		return this.$scope.global.extendedSlugArray.contains(word);
+	}
 
-  /**
-   * Checks if the given word in the mainSlugArray
-   */
-  checkWordInDB(word) {
-    return this.$scope.global.extendedSlugArray.contains(word);
-  }
+	/**
+	 * Checks if the given word is a verb.
+	 */
+	isVerb(word) {
+		let words = this.$scope.global.mainArray.filter((w) => {
+			return w.slug == word;
+		});
+		if (words.length > 0) {
+			if (words[0].type == "verb") {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
 
-  /**
-   * Checks if the given word is a verb.
-   */
-  isVerb(word) {
-    let words = this.$scope.global.mainArray.filter((w) => {
-      return w.slug == word;
-    });
-    if (words.length > 0) {
-      if (words[0].type == "verb") {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      return false;
-    }
-  }
+	/**
+	 * Turn suggested word into its slug
+	 */
+	slugSuggest(suggestWord) {
+		return this.$scope.global.extendedSlugMap[suggestWord];
+	}
 
-  /**
-   * Turn suggested word into its slug
-   */
-  slugSuggest(suggestWord) {
-    return this.$scope.global.extendedSlugMap[suggestWord];
-  }
-
-  /**
-   * Comparission function for .sort() prototype.
-   */
-  sortByLength(a, b) {
-    if (a.length > b.length) {
-      return 1;
-    }
-    if (a.length < b.length) {
-      return -1;
-    }
-    return 0;
-  }
+	/**
+	 * Comparission function for .sort() prototype.
+	 */
+	sortByLength(a, b) {
+		if (a.length > b.length) {
+			return 1;
+		}
+		if (a.length < b.length) {
+			return -1;
+		}
+		return 0;
+	}
 
 }
 
