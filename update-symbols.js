f2663178c92e1b0f84348b3d3210222147d41b1d
Burak Tokak <iletisim@buraktokak.com>
2016-10-21 17:39:09 +0300

    Change directory name in symbols update (update-symbols)

--- update-symbols.js
+++ update-symbols.js
@@ -2,12 +2,16 @@ var exec = require('child_process').exec;
 
 var langArr = ["en", "tr"];
 langArr.forEach(function(lan) {
-	exec('rm -rf app/symbols/aac-' + lan + '/', function(err, stdo, stde) {
+	exec('rm -rf app/symbols/' + lan + '/', function(err, stdo, stde) {
 		console.log("Old files removed!");
 		console.log("Cloning..");
 		exec('git clone git@bitbucket.org:otsimo/aac-' + lan + '.git', { cwd: "app/symbols" }, function(err, stdo, stde) {
 			console.log(stdo);
 			console.log(lan + " Loaded!!");
+			exec('mv app/symbols/aac-' + lan + ' app/symbols/' + lan + '', function(err, stdo, stde) {
+				console.log(err);
+				console.log(lan + " Moved!!!");
+			});
 		});
 	});
 });
