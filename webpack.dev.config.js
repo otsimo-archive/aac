bb5c7965b909c6b563e0aef4a723cd1d065ed600
btk <iletisim@buraktokak.com>
2016-12-12 14:12:04 +0300

    Responsive voice tts injected from local only for development.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -8,65 +8,64 @@ let sourcePath = path.resolve(__dirname, 'app')
 let otsimoPath = path.resolve(__dirname, 'node_modules', 'otsimo')
 let initPath = path.resolve(__dirname, 'app', 'js');
 let fastClickPath = path.resolve(__dirname, 'node_modules', 'fastclick', 'lib')
+let responsivevoicePath = path.resolve(__dirname, 'app', 'js')
 
 module.exports = {
-  devtool: 'source-map',
-  cache: true,
-  entry: {
-    'vendor': ['angular', 'angular-mocks', 'ngtouch'],
-    'app': path.join(sourcePath, 'app.js')
-  },
-  output: {
-    path: outputPath,
-    filename: '[name].js',
-    sourceMapFilename: '[name].map',
-    chunkFilename: '[id].chunk.js'
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      loader: 'babel',
-      query: {
-        presets: ['es2015']
-      }
-    }]
-  },
-  resolve: {
-    root: [sourcePath],
-    modules: [
-      'node_modules',
-      sourcePath
+    devtool: 'source-map',
+    cache: true,
+    entry: {
+        'vendor': ['angular', 'angular-mocks', 'ngtouch'],
+        'app': path.join(sourcePath, 'app.js')
+    },
+    output: {
+        path: outputPath,
+        filename: '[name].js',
+        sourceMapFilename: '[name].map',
+        chunkFilename: '[id].chunk.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    },
+    resolve: {
+        root: [sourcePath],
+        modules: [
+            'node_modules',
+            sourcePath
+        ]
+    },
+    devServer: {
+        progress: true,
+        contentBase: outputPath,
+        outputPath: outputPath
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor'],
+            minChunks: Infinity
+        }),
+        new CopyWebpackPlugin([{
+            context: sourcePath,
+            from: '**/*.{html,css,woff,json,svg}'
+        }, {
+            context: otsimoPath,
+            from: 'otsimo.js'
+        }, {
+            context: initPath,
+            from: 'init.js'
+        }, {
+            context: fastClickPath,
+            from: 'fastclick.js'
+        }, {
+            context: responsivevoicePath,
+            from: 'responsivevoice.js'
+        }]),
+        new webpack.NoErrorsPlugin()
     ]
-  },
-  devServer: {
-    progress: true,
-    contentBase: outputPath,
-    outputPath: outputPath
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ['app', 'vendor'],
-      minChunks: Infinity
-    }),
-    new CopyWebpackPlugin([
-      {
-        context: sourcePath,
-        from: '**/*.{html,css,woff,json,svg}'
-      },
-      {
-        context: otsimoPath,
-        from: 'otsimo.js'
-      },
-      {
-        context: initPath,
-        from: 'init.js'
-      },
-      {
-        context: fastClickPath,
-        from: 'fastclick.js'
-      }
-    ]),
-    new webpack.NoErrorsPlugin()
-  ]
 }
