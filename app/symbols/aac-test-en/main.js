cea5647ff4689137d6a32394e7b1e93ef2f6fc11
Burak Tokak <iletisim@buraktokak.com>
2017-02-01 15:59:13 +0300

    Create language plugin class interface in test-en/main.js

--- app/symbols/aac-test-en/main.js
+++ app/symbols/aac-test-en/main.js
@@ -1,19 +1,19 @@
 export default class LanguagePack {
     constructor() {
-        this.name = "en";
-        this.poss = [
-            "i",
-            "you",
-            "he",
-            "she",
-            "it",
-            "they",
-            ""
-        ];
+        this.name = "test-en";
+        this.poss = [""];
+        this.conjtype = [""];
+        this.nounCondition = [""];
     }
 
-    conj() {
-        return "111";
+    conjVerb(verb, tence, poss) {
+        // Verb conjunction function
     }
 
+    conjNoun(noun, type) {
+        // Noun condition function
+    }
+    addPoss(verb, poss, tence, identifier) {
+        // Possessor function
+    }
 }
