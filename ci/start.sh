d213b8123748365ab5fe6ee6469b011ff9ee7e69
Sercan Degirmenci <degirmencisercan@gmail.com>
2020-01-28 11:32:12 +0300

    update dockerfile

--- ci/start.sh
+++ ci/start.sh
@@ -2,7 +2,7 @@
 
 export OTSIMO_ROOT_DIR="${OTSIMO_ROOT_DIR:-/opt/otsimo-games}"
 
-echo "clone '$OTSIMO_GIT_BRANCH' brach of '$OTSIMO_GIT_REPOSITORY' repository to $OTSIMO_ROOT_DIR"
+echo "clone '$OTSIMO_GIT_BRANCH' branch of '$OTSIMO_GIT_REPOSITORY' repository to $OTSIMO_ROOT_DIR"
 git clone -b $OTSIMO_GIT_BRANCH $OTSIMO_GIT_REPOSITORY $OTSIMO_ROOT_DIR
 
 cd $OTSIMO_ROOT_DIR
